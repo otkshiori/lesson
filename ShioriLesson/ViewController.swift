d8b206cfc39ba2f6a50921b638e738e8398216ed
ほたる <hotaru@hotaru-no-MacBook-Air.local>
2018-09-18 21:14:51 +0900

    first commit

--- ShioriLesson/ViewController.swift
+++ ShioriLesson/ViewController.swift
@@ -8,18 +8,47 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet weak var textField: UITextField! {
+        didSet {
+            textField.delegate = self
+        }
+    }
+    @IBOutlet weak var answerLabel: UILabel!
+    
+    let dictionary: [String : String] = [
+        "すずき" : "けんた" ,
+        "おおつか" : "しおり"
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func buttonAccsion() {
+        showAnswer()
+    }
+    
+    func showAnswer() {
+        let inputText: String = textField.text!
+        
+        if let name = dictionary[inputText] {
+            answerLabel.text = name
+        } else {
+            answerLabel.text = "お留守です"
+        }
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        showAnswer()
+        return true
+    }
 }
 
